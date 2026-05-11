320beebe3324a95f0d8c30ef74ef1b149e8ee287
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2026-05-11 11:15:06 -0300

    Setup s3 for produciton

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  config.active_storage.service = :amazon
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
@@ -22,7 +22,7 @@ Rails.application.configure do
   # config.asset_host = "http://assets.example.com"
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # config.assume_ssl = true
